be0b37e1e8bc15e5ccc09a6b63c5004d40757436
santiagomanso <santiagomanso@web.de>
2023-04-26 23:07:53 +0200

    🎨 added css tailwind, fontawesome icons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,10 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import '../styles/globals.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+library.add(fas)
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
