95c2e377f8fe3ca4a9f4dfe848109a02e121d0d4
santiagomanso <santiagomanso@web.de>
2023-04-26 23:18:34 +0200

    📝 update/ documentation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import { pokemons } from '@/data/pokemonsData'
 import { useState } from 'react'
 
 export default function Home() {
+  //FIXME - use context instead of internal state
   const [active, setactive] = useState(false)
 
   console.log('pokemons.pokemons', pokemons.pokemons)
@@ -14,6 +15,7 @@ export default function Home() {
       <Input />
       <div>
         <ul className='flex gap-1 w-full mt-5'>
+          {/* we use the array of sample Pokemons to display an array of dragable <li> */}
           {pokemons.pokemons.map((pokemon) => (
             <DragPokemon pokemon={pokemon.pokemon} key={pokemon.pokemon.id} />
           ))}
