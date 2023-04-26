5511d08848d11f714098f98b9aee1c3688fc23d3
santiagomanso <santiagomanso@web.de>
2023-04-26 23:07:14 +0200

    🗃️ created/ sample data

---
+++ src/data/pokemonsData.ts
@@ -0,0 +1,10 @@
+import { pokemonList, pokemonI } from '@/interfaces/interfaces'
+
+export const pokemons: pokemonList = {
+  pokemons: [
+    { pokemon: { id: 1, name: 'pikachu', type: 'electric' } },
+    { pokemon: { id: 2, name: 'Bulbasaur', type: 'grass' } },
+    { pokemon: { id: 3, name: 'Charmander', type: 'fire' } },
+    { pokemon: { id: 4, name: 'Squirtle', type: 'water' } },
+  ],
+}
