6170771937bcbc5407d3b6afb0a7c9e6a55992b7
santiagomanso <santiagomanso@web.de>
2023-05-08 23:24:15 +0200

    🔧 reworked order

--- src/data/pokemonsData.ts
+++ src/data/pokemonsData.ts
@@ -5,7 +5,8 @@ export const pokemons: pokemonList = {
   pokemons: [
     { pokemon: { id: 1, name: 'pikachu', type: 'electric' } },
     { pokemon: { id: 2, name: 'Bulbasaur', type: 'grass' } },
-    { pokemon: { id: 3, name: 'Charmander', type: 'fire' } },
-    { pokemon: { id: 4, name: 'Squirtle', type: 'water' } },
+    { pokemon: { id: 3, name: 'Squirtle', type: 'water' } },
+    { pokemon: { id: 4, name: 'Charmander', type: 'fire' } },
+    { pokemon: { id: 4, name: 'Snorlax', type: 'normal' } },
   ],
 }
