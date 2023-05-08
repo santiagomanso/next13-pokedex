99e40daffdd1271da62e2f1aa9f7b91e873384ff
santiagomanso <santiagomanso@web.de>
2023-05-08 23:24:06 +0200

    🚧 commented out old components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,21 @@
 import Container from '@/components/Container'
 import DragPokemon from '@/components/DragPokemon'
 import Input from '@/components/Input'
+import Section from '@/components/Section'
 import { pokemons } from '@/data/pokemonsData'
-import { useState } from 'react'
 
 export default function Home() {
-  //FIXME - use context instead of internal state
-  const [active, setactive] = useState(false)
-
-  console.log('pokemons.pokemons', pokemons.pokemons)
-
   return (
     <Container>
-      <Input />
-      <div>
+      {/* <Input /> 
+       <div>
         <ul className='flex gap-1 w-full mt-5'>
-          {/* we use the array of sample Pokemons to display an array of dragable <li> */}
           {pokemons.pokemons.map((pokemon) => (
             <DragPokemon pokemon={pokemon.pokemon} key={pokemon.pokemon.id} />
           ))}
         </ul>
-      </div>
+      </div> */}
+      <Section />
     </Container>
   )
 }
