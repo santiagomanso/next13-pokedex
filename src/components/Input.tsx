0405886edbf9470080a982247303c28786fea8b5
santiagomanso <santiagomanso@web.de>
2023-05-10 10:36:30 +0200

    🔧 reworked component, click span set global state

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,7 +1,9 @@
+import { SearchContext } from '@/context/SearchContext'
 import React, { useContext, useState } from 'react'
 
 const Input = () => {
   const [active, setActive] = useState('')
+  const { searchPokemon, setSearchPokemon } = useContext(SearchContext)
 
   const handleDragOver = (e: React.DragEvent<HTMLInputElement>) => {
     e.preventDefault()
@@ -14,7 +16,7 @@ const Input = () => {
   }
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setActive(e.target.value)
+    setSearchPokemon(e.target.value)
   }
 
   return (
@@ -25,7 +27,7 @@ const Input = () => {
         type='text'
         placeholder='search for a pokemon'
         className='px-2 py-2 focus:outline-none bg-transparent border-2 border-gray-400 text-gray-500 font-medium placeholder-gray-500 uppercase shadow-lg rounded w-full text-center'
-        value={active}
+        value={searchPokemon}
         onChange={handleChange}
       />
     </>
