283baf735671ca7145620cf4feb28cfdc30b48d2
santiagomanso <santiagomanso@web.de>
2023-05-10 09:15:53 +0200

    🔨 added layout & footer with fragment

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,21 @@ import '../styles/globals.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Navbar from '@/components/Navbar'
+
+import Footer from '@/components/Footer'
+import Layout from '@/components/Layout'
 
 library.add(fas)
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      <Footer />
+    </>
+  )
 }
