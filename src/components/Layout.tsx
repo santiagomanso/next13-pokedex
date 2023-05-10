641e74b71534676e6570dd4aec6457c279136c66
santiagomanso <santiagomanso@web.de>
2023-05-10 09:15:09 +0200

    🔨 created component

---
+++ src/components/Layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+interface LayoutI {
+  children: React.ReactNode
+}
+
+const Layout = ({ children }: LayoutI) => {
+  return (
+    <main className='bg-gradient-to-br from-slate-500 to-gray-200 flex justify-center lg:items-center h-screen'>
+      <section className='w-full max-w-7xl h-1/2 border-2 border-neutral-500 rounded-md shadow-2xl shadow-slate-500'>
+        {children}
+      </section>
+    </main>
+  )
+}
+
+export default Layout
