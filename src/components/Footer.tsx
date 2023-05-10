8fba91edd8829711afb08b3035601236b240e133
santiagomanso <santiagomanso@web.de>
2023-05-10 09:14:52 +0200

    🔨created component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Footer = (props: Props) => {
+  return (
+    <footer className='fixed bottom-2 left-0 w-full text-center'>
+      By Santiago Manso Castro
+    </footer>
+  )
+}
+
+export default Footer
