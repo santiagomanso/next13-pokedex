573e7a87a640a4839a944d1da1a726901a21ac04
santiagomanso <santiagomanso@web.de>
2023-05-11 20:16:07 +0200

    🚧 added drag n drop provider to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,29 +1,41 @@
 import type { AppProps } from 'next/app'
+
+//import for tailwind
 import '../styles/globals.css'
+
+//imports for fontawesome icons
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import Navbar from '@/components/Navbar'
 
+//imports for component
+import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 import Layout from '@/components/Layout'
+
+//imports for Context-API
 import { SearchContextProvider } from '@/context/SearchContext'
 import { DragContextProvider } from '@/context/dragContext'
+
+//imports from react-drag-n-drop
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 
 library.add(fas)
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <SearchContextProvider>
-        <DragContextProvider>
-          <Navbar />
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-          <Footer />
-        </DragContextProvider>
-      </SearchContextProvider>
+      <DndProvider backend={HTML5Backend}>
+        <SearchContextProvider>
+          <DragContextProvider>
+            <Navbar />
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+            <Footer />
+          </DragContextProvider>
+        </SearchContextProvider>
+      </DndProvider>
     </>
   )
 }
