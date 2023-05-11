7b2bf6392a2a05b77442f429e98e0571ab433ce9
santiagomanso <santiagomanso@web.de>
2023-05-11 23:48:26 +0200

    ⚰️ removed drag&drop functionallity

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -4,32 +4,18 @@ import React, { useContext } from 'react'
 const Input = () => {
   const { searchPokemon, setSearchPokemon } = useContext(SearchContext)
 
-  const handleDragOver = (e: React.DragEvent<HTMLInputElement>) => {
-    e.preventDefault()
-  }
-
-  const handleDrop = (e: React.DragEvent<HTMLInputElement>) => {
-    e.preventDefault()
-    const pokemonName = e.dataTransfer.getData('text/plain')
-    setSearchPokemon(pokemonName)
-  }
-
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchPokemon(e.target.value)
   }
 
   return (
-    <>
-      <input
-        onDragOver={handleDragOver}
-        onDrop={handleDrop}
-        type='text'
-        placeholder='search for a pokemon'
-        className='px-2 py-2 focus:outline-none bg-transparent border-2 border-gray-400 text-gray-500 font-medium placeholder-gray-500 uppercase shadow-lg rounded w-full text-center'
-        value={searchPokemon}
-        onChange={handleChange}
-      />
-    </>
+    <input
+      type='text'
+      placeholder='search for a pokemon'
+      className='px-2 py-2 focus:outline-none text-gray-500 font-medium placeholder-gray-500 uppercase shadow-xl rounded w-full text-center'
+      value={searchPokemon}
+      onChange={handleChange}
+    />
   )
 }
 
